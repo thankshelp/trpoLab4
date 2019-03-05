4c2e5a727b6a1c2ac298b43e9412087f86875d0c
LittelHell13 <hottelmich@gmail.com>
2019-03-05 13:16:20 +0700

    add

--- Lab4/MainWindow.xaml.cs
+++ Lab4/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,59 @@ namespace Lab4
     /// </summary>
     public partial class MainWindow : Window
     {
+        string c;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void add_button_Click(object sender, RoutedEventArgs e)
+        {
+            c = vod.Text;
+
+            stack.Items.Add(c);
+
+            vod.Text = "";
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Document";
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text document (.txt)|*.txt";
+            dlg.ShowDialog();
+
+            string[] l = stack.Items.OfType<string>().ToArray();
+
+            using (StreamWriter outputFile = new StreamWriter(dlg.FileName))
+            {
+                foreach (string line in l)
+                    outputFile.WriteLine(line);
+                
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document";
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text document (.txt)|*.txt";
+            dlg.ShowDialog();
+
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(dlg.FileName);
+
+            while((line = file.ReadLine()) != null)
+            {
+                stack.Items.Add(line);
+            }
+            file.Close();
+        }
+
+        
     }
 }
